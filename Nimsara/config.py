9688ac7cf94633ddad03bf1b5df578238f1592b7
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-12-10 22:54:25 +0530

    Update config.py

--- Nimsara/config.py
+++ Nimsara/config.py
@@ -9,9 +9,9 @@ MONGO_URI = "mongodb+srv://klgeethika:<klgeethika>@cluster0.u3oatsp.mongodb.net/
 BOT_USERNAME = "Nimsaraxbot"
 
 id_pattern = re.compile(r'^.\d+$')
-REQ_ARIA= int(-1001693331508)
+REQ_ARIA= int(-1002062161170)
 CHAT = [int(ch) if id_pattern.search(ch) else ch for ch in '-1001879497983 -1001396114028 -1001375013540'.split()]
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in '1361226141 1746346543 6363073939'.split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in '1949037588 1746346543 6363073939'.split()]
 
 START_TEXT = "Hey {} I'm ALive"
 START_BUTTON = InlineKeyboardMarkup(
