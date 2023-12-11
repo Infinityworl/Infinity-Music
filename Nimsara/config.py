407190deba5c198de45a087d3bba957a5e5f002b
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-12-11 11:06:45 +0530

    Update config.py

--- Nimsara/config.py
+++ Nimsara/config.py
@@ -13,8 +13,7 @@ REQ_ARIA= int(-1002062161170)
 CHAT = [int(ch) if id_pattern.search(ch) else ch for ch in '-1001879497983 -1001396114028 -1001375013540'.split()]
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in '1949037588 1746346543 6363073939'.split()]
 
-START_TEXT = "Hey {} ᴅᴇᴀʀ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴍᴜꜱɪᴄ ᴡᴏʀʟᴅ
-ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴇɴᴊᴏʏ ʙᴇꜱᴛ ꜱᴏɴɢꜱ ɪɴ ᴏᴜʀ ʙᴏᴛ ☘️"
+START_TEXT = "Hey {} ᴅᴇᴀʀ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴍᴜꜱɪᴄ ᴡᴏʀʟᴅ ☘️"
 START_BUTTON = InlineKeyboardMarkup(
                             [[
                                     InlineKeyboardButton('ᴅᴇᴠᴇʟᴏᴘᴇʀ' , url="https://t.me/Nimsar_a"),
