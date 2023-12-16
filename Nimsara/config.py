0e5a3b695adee37ce418685a1bc7c6b962ab1bcf
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-12-16 21:41:46 +0530

    Update config.py

--- Nimsara/config.py
+++ Nimsara/config.py
@@ -13,7 +13,8 @@ REQ_ARIA= int(-1002062161170)
 CHAT = [int(ch) if id_pattern.search(ch) else ch for ch in '-1001885188788'.split()]
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in '1949037588'.split()]
 
-START_TEXT = "Hey {} ᴅᴇᴀʀ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴍᴜꜱɪᴄ ᴡᴏʀʟᴅ ☘️"
+START_TEXT = "Hey {} 🌸 ᴛʜɪꜱ ɪꜱ ᴍᴏꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ᴍᴜꜱɪᴄ ꜰɪɴᴅᴇʀ ʙᴏᴛ, ᴋᴇʏᴡᴏʀᴅ ꜱᴇᴀʀᴄʜᴇʀꜱ & ᴀʟꜱᴏ ᴠᴏɪᴄᴇ ꜱᴇᴀʀᴄʜᴇʀꜱ ꜱᴜᴘᴘᴏʀᴛᴇᴅ 🌷
+ ☘️"
 START_BUTTON = InlineKeyboardMarkup(
                             [[
                                     InlineKeyboardButton('ᴅᴇᴠᴇʟᴏᴘᴇʀ' , url="https://t.me/Nimsar_a"),
@@ -22,7 +23,7 @@ START_BUTTON = InlineKeyboardMarkup(
                             )
 
 st_start = 'CAACAgIAAxkBAAEoL9Nldq4aMAUhIoKg2lMSQ6OfZERpCgACAQEAAladvQoivp8OuMLmNDME'
-st_loading ='CAACAgUAAxkBAAEoL-dldrFPzxcocixG6zvPqSWGNjhtfAACmgADyJRkFCxl4eFc7yVqMwQ'
+st_loading ='CAACAgIAAx0CbhArBwACGAplfcuyOI9ys_e8Tqx9iPyhVD4GCgAC6BYAAv2LEEra9hZZ9LdRQB4E'
 st_downloading = 'CAACAgEAAxkBAAEoMFNldsx2wvR-Lfgn--vsCqFpdoCl3QACBAYAAm5PKEe-rP_0rU2xITME'
 st_uploading = 'CAACAgUAAxkBAAEoMCNldr65MpR0gSM08Dc5UHUJv9Is1gACpQADyJRkFHhDhV4BRbZGMwQ'
 st_done= 'CAACAgUAAxkBAAEoL_9ldroNVKslyAv7kU28qs2aF3j7JwACpAADyJRkFIBDD5aPWWn6MwQ'
