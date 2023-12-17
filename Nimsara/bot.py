eb2d86cca7333d1d4cf1c6e067ec8c311a389e84
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-18 04:31:18 +0530

    Update bot.py

--- Nimsara/bot.py
+++ Nimsara/bot.py
@@ -48,7 +48,7 @@ async def start(_, message):
         try:
             await bot.send_photo(
                 message.from_user.id,
-                "https://i.ibb.co/Bcx5564/image.png",
+                "https://i.ibb.co/3zvHYPh/photo-2023-08-30-22-45-44.jpg",
                 caption = START_TEXT.format(message.from_user.mention),
                 reply_markup = START_BUTTON)
         except Exception as e:
@@ -57,8 +57,8 @@ async def start(_, message):
         return await add_served_chat(message.chat.id) 
     else:
         await bot.send_photo(
-            photo='https://i.ibb.co/Bcx5564/image.png',
-            chat_id=message.from_user.id,
+            message.from_user.id,
+            "https://i.ibb.co/3zvHYPh/photo-2023-08-30-22-45-44.jpg",
             caption = START_TEXT.format(message.from_user.mention),
             reply_markup = START_BUTTON)
     return await add_served_user(message.from_user.id)
