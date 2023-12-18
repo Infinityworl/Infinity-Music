2921e696c5b7ba90d6e42ef12df33e5e9138499b
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-19 00:34:11 +0530

    Update start.py

--- Nimsara/Plugins/start.py
+++ Nimsara/Plugins/start.py
@@ -1,5 +1,17 @@
-from telegram import Update
+from telegram import Update , InlineKeyboardButton , InlineKeyboardMarkup
 from telegram.ext import ContextTypes
+from config import BOT_USERNAME , img_start
+from Plugins.help_functions.mention import mention
 
 async def start_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello there! I\'m a bot. What\'s up?')
+    user_id = update.effective_user.id
+    user = await mention(user_id,context)
+    photo = img_start
+    caption = f"ʜᴇʏ {user} 🌸 ᴛʜɪꜱ ɪꜱ ᴍᴏꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ᴍᴜꜱɪᴄ ꜰɪɴᴅᴇʀ ʙᴏᴛ, ᴋᴇʏᴡᴏʀᴅ ꜱᴇᴀʀᴄʜᴇʀꜱ & ᴀʟꜱᴏ ᴠᴏɪᴄᴇ ꜱᴇᴀʀᴄʜᴇʀꜱ ꜱᴜᴘᴘᴏʀᴛᴇᴅ 🌷"
+    inline_keyboard = [
+        [InlineKeyboardButton('⭕ 𝐂𝐡𝐚𝐭 𝐆𝐫𝐨𝐮𝐩 ⭕ʟ', url=f"https://t.me/Musicx_lk"),InlineKeyboardButton('✨️ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫 ✨️', url=f"https://t.me/nimsar_a")],
+        [InlineKeyboardButton('🌸 ᴀᴅᴅ ᴍᴇ ʙᴇᴀᴜᴛʏ 🌸', url=f"http://t.me/{BOT_USERNAME}?startgroup=true")],
+        [InlineKeyboardButton('🌼 ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ 🌼', url=f"https://t.me/sinhalafilx")]
+            ]
+    reply_markup=InlineKeyboardMarkup(inline_keyboard)
+    await update.message.reply_photo(caption=caption,photo=photo,reply_markup=reply_markup,parse_mode='Markdown')
