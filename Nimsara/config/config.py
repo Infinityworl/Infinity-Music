cc462e79b3df0cd6f120eef33da9450fbebf9f32
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-18 23:51:00 +0530

    Update config.py

--- Nimsara/config/config.py
+++ Nimsara/config/config.py
@@ -1,9 +1,8 @@
 import re
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-APPID = "24008761"
-APIHASH = "c6bb29db832216220e1234b163233cec"
-BOTTOKEN = "6578576818:AAEBfTyvp6u_oCXQ0d_kbslNg0SEFkgQvtA"
+
+TOKEN = "6578576818:AAEBfTyvp6u_oCXQ0d_kbslNg0SEFkgQvtA"
 
 MONGO_URI = "mongodb+srv://nimsara:nimsara@cluster0.mgn7qqv.mongodb.net/?retryWrites=true&w=majority"
 BOT_USERNAME = "Nimsaraxbot"
@@ -29,8 +28,10 @@ START_BUTTON = InlineKeyboardMarkup(
                 ]
 )
 img_start = "https://i.ibb.co/3zvHYPh/photo-2023-08-30-22-45-44.jpg"
-st_start = 'CAACAgIAAxkBAAEoL9Nldq4aMAUhIoKg2lMSQ6OfZERpCgACAQEAAladvQoivp8OuMLmNDME'
-st_loading ='CAACAgIAAx0CbhArBwACGAplfcuyOI9ys_e8Tqx9iPyhVD4GCgAC6BYAAv2LEEra9hZZ9LdRQB4E'
+st_loading ='CAACAgUAAxkBAAEoL-dldrFPzxcocixG6zvPqSWGNjhtfAACmgADyJRkFCxl4eFc7yVqMwQ'
+st_downloading = 'CAACAgUAAxkBAAEoMAlldrwZJuyZvCYdaqdPIEKgMAvFEwACnQADyJRkFKod8wepbhc6MwQ'
+st_uploading = 'CAACAgUAAxkBAAEoMCNldr65MpR0gSM08Dc5UHUJv9Is1gACpQADyJRkFHhDhV4BRbZGMwQ'
+st_done= 'CAACAgUAAxkBAAEoL_9ldroNVKslyAv7kU28qs2aF3j7JwACpAADyJRkFIBDD5aPWWn6MwQ'
 st_downloading = 'CAACAgEAAxkBAAEoMFNldsx2wvR-Lfgn--vsCqFpdoCl3QACBAYAAm5PKEe-rP_0rU2xITME'
 st_uploading = 'CAACAgUAAxkBAAEoMCNldr65MpR0gSM08Dc5UHUJv9Is1gACpQADyJRkFHhDhV4BRbZGMwQ'
 st_done= 'CAACAgUAAxkBAAEoL_9ldroNVKslyAv7kU28qs2aF3j7JwACpAADyJRkFIBDD5aPWWn6MwQ'
