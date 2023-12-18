79b4d8a37fb674890c0328dd5b8be80ce9def99f
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-19 03:49:35 +0530

    Update help.py

--- Nimsara/Plugins/help.py
+++ Nimsara/Plugins/help.py
@@ -1,5 +1,8 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from Plugins.help_functions.force_sub import force_sub_chanel
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Try typing anything, and I will do my best to respond.')
+    in_channel = await force_sub_chanel(update,context)
+    if in_channel:
+        await update.message.reply_text('Try typing anything, and I will do my best to respond.')
