c00ffe22e3bf10460f63f8132ad248532938bfa5
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-19 00:10:42 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -1,16 +1,9 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from Plugins import handle_song
+from Plugins import handle_song , start_command , help_command
 from config import TOKEN
 
 print('Starting up bot...')
-
-
-async def start_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello there! I\'m a bot. What\'s up?')
-def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    update.message.reply_text('Try typing anything, and I will do my best to respond.')
-
 
 
 def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -18,7 +11,6 @@ def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('song', handle_song))
