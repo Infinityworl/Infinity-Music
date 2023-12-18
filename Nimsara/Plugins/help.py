4576c2137f9307c2ff26d46390f36d78131b7fcc
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-19 02:15:39 +0530

    Update help.py

--- Nimsara/Plugins/help.py
+++ Nimsara/Plugins/help.py
@@ -1,5 +1,5 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    update.message.reply_text('Try typing anything, and I will do my best to respond.')
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Try typing anything, and I will do my best to respond.')
