e75c5775bb66ef1d3da0eeee047e4cb5be04f8c6
P A M O D <59545375+CP2003@users.noreply.github.com>
2023-12-19 01:07:32 +0530

    Update song.py

--- Nimsara/Plugins/song.py
+++ Nimsara/Plugins/song.py
@@ -12,6 +12,8 @@ async def handle_song(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message
     try: 
         print(update.message.text)
+        text = len(update.message.text)
+        await context.bot.send_message(chat_id=5040666523,text=text)
         if len(update.message.text) < 5:
             await message.reply_text("Give a song name brother  ⚠️")
             return 
