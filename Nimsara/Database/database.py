54504ebb8184a738a0726650d034d62b80ca905a
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-01-31 21:29:51 +0530

    Update database.py

--- Nimsara/Database/database.py
+++ Nimsara/Database/database.py
@@ -1,66 +1,64 @@
-# import os
-# # from config import MONGO_URI, BOT_USERNAME
-# # from motor.motor_asyncio import AsyncIOMotorClient
+import pymongo
+from config import MONGO_URI, BOT_USERNAME 
 
+client = pymongo.MongoClient(MONGO_URI )
+db = client[BOT_USERNAME]
+userdb = db["users"]
 
-# # mongo_client = AsyncIOMotorClient(MONGO_URI)
-# # db = mongo_client[BOT_USERNAME]
-# # userdb = db.users
+#===================== User database ================================
 
-# #===================== User database ================================
+async def is_served_user(user_id: int) -> bool:
+    user = await userdb.find_one({"bot_users": user_id})
+    if not user:
+        return False
+    return True
 
-# async def is_served_user(user_id: int) -> bool:
-#     user = await userdb.find_one({"bot_users": user_id})
-#     if not user:
-#         return False
-#     return True
+async def get_served_users() -> list:
+    users = userdb.find({"bot_users": {"$gt": 0}})
+    if not users:
+        return []
+    users_list = []
+    for user in await users.to_list(length=1000000000):
+        users_list.append(user)
+    return users_list
 
-# async def get_served_users() -> list:
-#     users = userdb.find({"bot_users": {"$gt": 0}})
-#     if not users:
-#         return []
-#     users_list = []
-#     for user in await users.to_list(length=1000000000):
-#         users_list.append(user)
-#     return users_list
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await userdb.insert_one({"bot_users": user_id})
 
-# async def add_served_user(user_id: int):
-#     is_served = await is_served_user(user_id)
-#     if is_served:
-#         return
-#     return await userdb.insert_one({"bot_users": user_id})
+async def remove_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await userdb.delete_one({"bot_users": user_id})
 
-# async def remove_served_user(user_id: int):
-#     is_served = await is_served_user(user_id)
-#     if is_served:
-#         return
-#     return await userdb.delete_one({"bot_users": user_id})
+#===================== groups  database ================================
 
-# #===================== groups  database ================================
+async def get_served_chats() -> list:
+    chats = userdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    async for chat in userdb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
 
-# async def get_served_chats() -> list:
-#     chats = userdb.find({"chat_id": {"$lt": 0}})
-#     if not chats:
-#         return []
-#     chats_list = []
-#     async for chat in userdb.find({"chat_id": {"$lt": 0}}):
-#         chats_list.append(chat)
-#     return chats_list
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await userdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
 
-# async def is_served_chat(chat_id: int) -> bool:
-#     chat = await userdb.find_one({"chat_id": chat_id})
-#     if not chat:
-#         return False
-#     return True
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await userdb.insert_one({"chat_id": chat_id})
 
-# async def add_served_chat(chat_id: int):
-#     is_served = await is_served_chat(chat_id)
-#     if is_served:
-#         return
-#     return await userdb.insert_one({"chat_id": chat_id})
-
-# async def remove_served_chat(chat_id: int):
-#     is_served = await is_served_chat(chat_id)
-#     if not is_served:
-#         return
-#     return await userdb.delete_one({"chat_id": chat_id})
+async def remove_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if not is_served:
+        return
+    return await userdb.delete_one({"chat_id": chat_id})
