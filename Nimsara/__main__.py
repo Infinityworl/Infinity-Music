e30f89de4692ff51ffeea2543b56c13afc8919c9
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-01-31 21:07:40 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -12,7 +12,7 @@ def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
-    # app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('start', start_command))
     # app.add_handler(CommandHandler('help', help_command))
     # app.add_handler(CommandHandler('song', handle_song))
     app.add_error_handler(error)
