aea1b9fea28214f79835b530a43f793c21b5b536
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-01-31 21:03:04 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     # app.add_handler(CommandHandler('song', handle_song))
     app.add_error_handler(error)
     print('Polling...')
-    app.run_polling(poll_interval=5)
+    app.run_polling(poll_interval=3)
