32ebb0a48ddb94b6454b60e7e7d20f96b2f0b35b
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-01-31 21:55:18 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from Plugins import start_command ,error_handler
+from Plugins import start_command ,error_handler , song
 from config import TOKEN
 
 print('Starting up bot...')
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
     # app.add_handler(CommandHandler('help', help_command))
-    # app.add_handler(CommandHandler('song', handle_song))
+    app.add_handler(CommandHandler('song', song))
     app.add_error_handler(error_handler)
     print('Polling...')
     app.run_polling(poll_interval=3)
