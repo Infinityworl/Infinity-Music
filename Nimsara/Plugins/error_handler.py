4b216c69af1e7e03707273d5e374ac5712030c51
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-01-31 21:47:38 +0530

    Update error_handler.py

--- Nimsara/Plugins/error_handler.py
+++ Nimsara/Plugins/error_handler.py
@@ -1,1 +1,40 @@
+"""ᴇʀʀᴏʀ ʜᴀɴᴅʟᴇʀ ᴠ𝟸.𝟶𝟷 
+   ├ ᴄᴏᴘʏʀɪɢʜᴛ © 𝟸𝟶𝟸𝟹-𝟸𝟶𝟸𝟺 ᴘᴀᴍᴏᴅ ᴍᴀᴅᴜʙᴀsʜᴀɴᴀ. ᴀʟʟ ʀɪɢʜᴛs ʀᴇsᴇʀᴠᴇᴅ.
+   ├ ʟɪᴄᴇɴsᴇᴅ ᴜɴᴅᴇʀ ᴛʜᴇ  ɢᴘʟ-𝟹.𝟶 ʟɪᴄᴇɴsᴇ.
+   └ ʏᴏᴜ ᴍᴀʏ ɴᴏᴛ ᴜsᴇ ᴛʜɪs ғɪʟᴇ ᴇxᴄᴇᴘᴛ ɪɴ ᴄᴏᴍᴘʟɪᴀɴᴄᴇ ᴡɪᴛʜ ᴛʜᴇ ʟɪᴄᴇɴsᴇ.
+"""
 
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def price_error(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    e = context.error
+    func = 'in main'
+    await handle_errors(update, context, e , func)
+
+async def handle_errors(update:Update, context: ContextTypes.DEFAULT_TYPE, e , func):
+    user = update.effective_user
+    chat = update.effective_chat
+    message = update.effective_message
+    error_log = ''
+    e = str(e)
+    e = await block_text(e)
+
+    error_log += f'\n> ᴜsᴇʀ : {user.first_name}'
+    error_log += f'\n> ᴜsᴇʀ ɪᴅ :{user.id}'
+
+
+
+    if chat.id != user.id:
+        error_log += f'\n> ᴄʜᴀᴛ : {chat.title}'
+        error_log += f'\n> ᴄʜᴀᴛ ɪᴅ : {chat.id}'
+        error_log += f'\n> ᴄʜᴀᴛ ᴛʏᴘᴇ : {chat.type}'
+        
+    error_log += f'\n> ғᴜɴᴄᴛɪᴏɴ : {func}'
+    if message.caption:
+        error_log += f'\n> ᴄᴀᴘᴛɪᴏɴ : {message.caption}'
+    else:
+        error_log += f'\n> ᴍᴇssᴀɢᴇ : {message.text}'
+    
+    error_text = f'```Error {e}``` \n\n```Update {error_log}```'
+    await context.bot.send_message(chat_id=-1001992131235, text=error_text,parse_mode='Markdown')
