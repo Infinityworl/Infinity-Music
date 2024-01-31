062766301592c588e949615350dbc350a3b4968f
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-01-31 21:49:03 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -1,18 +1,16 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from Plugins import start_command 
+from Plugins import start_command ,error_handler
 from config import TOKEN
 
 print('Starting up bot...')
 
-def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
 
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
     # app.add_handler(CommandHandler('help', help_command))
     # app.add_handler(CommandHandler('song', handle_song))
-    app.add_error_handler(error)
+    app.add_error_handler(error_handler)
     print('Polling...')
     app.run_polling(poll_interval=3)
