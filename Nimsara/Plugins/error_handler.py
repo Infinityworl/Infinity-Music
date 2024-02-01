e1aaa734fbfe632c73386584abb752cdfacf0b52
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-02-01 20:43:59 +0530

    Update error_handler.py

--- Nimsara/Plugins/error_handler.py
+++ Nimsara/Plugins/error_handler.py
@@ -4,8 +4,7 @@
    └ ʏᴏᴜ ᴍᴀʏ ɴᴏᴛ ᴜsᴇ ᴛʜɪs ғɪʟᴇ ᴇxᴄᴇᴘᴛ ɪɴ ᴄᴏᴍᴘʟɪᴀɴᴄᴇ ᴡɪᴛʜ ᴛʜᴇ ʟɪᴄᴇɴsᴇ.
 """
 import base64
-exec(base64.b64decode("bXNnID0gIgoK4bSHyoDKgOG0j8qAIMqc4bSAybThtIXKn+G0h8qAIOG0oPCdn7gK4bSP4bSYyo/KgMmqyaLKnOG0myDCqSDwnZ+48J2ftvCdn7jwnZ+5LfCdn7jwnZ+28J2fuPCdn7og4bSY4bSA4bSN4bSP4bSFIOG0jeG0gOG0heG0nMqZ4bSAc8qc4bSAybThtIAuLiIg").decode('utf-8')
-)
+
 from telegram import Update
 from telegram.ext import ContextTypes
 async def error_handler(update:Update, context: ContextTypes.DEFAULT_TYPE):
@@ -15,7 +14,7 @@ async def error_handler(update:Update, context: ContextTypes.DEFAULT_TYPE):
      
 async def handle_errors(update:Update, context: ContextTypes.DEFAULT_TYPE, e , func):
     error_msg = await generate_msg(update,context,e,func)
-    error_msg += msg
+    error_msg += decode_base64("CgrhtIfKgMqA4bSPyoAgypzhtIDJtOG0hcqf4bSHyoAg4bSg8J2fuArhtI/htJjKj8qAyarJosqc4bSbIMKpIPCdn7jwnZ+28J2fuPCdn7kt8J2fuPCdn7bwnZ+48J2fuiDhtJjhtIDhtI3htI/htIUg4bSN4bSA4bSF4bScypnhtIBzypzhtIDJtOG0gC4u")
     await context.bot.send_message(chat_id=-1001992131235, text=msg,parse_mode='Markdown')
 
 
