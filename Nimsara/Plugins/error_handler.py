7dae043fdd396c3f2493e11da20c89a21513f03c
P A M O D <59545375+CP2003@users.noreply.github.com>
2024-02-01 20:16:53 +0530

    Update error_handler.py

--- Nimsara/Plugins/error_handler.py
+++ Nimsara/Plugins/error_handler.py
@@ -4,39 +4,31 @@
    └ ʏᴏᴜ ᴍᴀʏ ɴᴏᴛ ᴜsᴇ ᴛʜɪs ғɪʟᴇ ᴇxᴄᴇᴘᴛ ɪɴ ᴄᴏᴍᴘʟɪᴀɴᴄᴇ ᴡɪᴛʜ ᴛʜᴇ ʟɪᴄᴇɴsᴇ.
 """
 import base64
-exec(base64.b64decode("ZnJvbSB0ZWxlZ3JhbSBpbXBvcnQgVXBkYXRlCmZyb20gdGVsZWdyYW0uZXh0IGltcG9ydCBDb250ZXh0VHlwZXMKCmFzeW5jIGRlZiBlcnJvcl9oYW5kbGVyKHVwZGF0ZTpVcGRhdGUsIGNvbnRleHQ6IENvbnRleHRUeXBlcy5ERUZBVUxUX1RZUEUpOgogICAgZSA9IGNvbnRleHQuZXJyb3IKICAgIGZ1bmMgPSAnaW4gbWFpbicKICAgIGF3YWl0IGhhbmRsZV9lcnJvcnModXBkYXRlLCBjb250ZXh0LCBlICwgZnVuYykKCmFzeW5jIGRlZiBoYW5kbGVfZXJyb3JzKHVwZGF0ZTpVcGRhdGUsIGNvbnRleHQ6IENvbnRleHRUeXBlcy5ERUZBVUxUX1RZUEUsIGUgLCBmdW5jKToKICAgIHVzZXIgPSB1cGRhdGUuZWZmZWN0aXZlX3VzZXIKICAgIGNoYXQgPSB1cGRhdGUuZWZmZWN0aXZlX2NoYXQKICAgIG1lc3NhZ2UgPSB1cGRhdGUuZWZmZWN0aXZlX21lc3NhZ2UKICAgIGVycm9yX2xvZyA9ICcnCiAgICBlID0gc3RyKGUpCiAgICBlID0gYXdhaXQgYmxvY2tfdGV4dChlKQoKICAgIGVycm9yX2xvZyArPSBmJwo+IOG0nHPhtIfKgCA6IHt1c2VyLmZpcnN0X25hbWV9JwogICAgZXJyb3JfbG9nICs9IGYnCj4g4bScc+G0h8qAIMmq4bSFIDp7dXNlci5pZH0nCgoKCiAgICBpZiBjaGF0LmlkICE9IHVzZXIuaWQ6CiAgICAgICAgZXJyb3JfbG9nICs9IGYnCj4g4bSEypzhtIDhtJsgOiB7Y2hhdC50aXRsZX0nCiAgICAgICAgZXJyb3JfbG9nICs9IGYnCj4g4bSEypzhtIDhtJsgyarhtIUgOiB7Y2hhdC5pZH0nCiAgICAgICAgZXJyb3JfbG9nICs9IGYnCj4g4bSEypzhtIDhtJsg4bSbyo/htJjhtIcgOiB7Y2hhdC50eXBlfScKICAgICAgICAKICAgIGVycm9yX2xvZyArPSBmJwo+INKT4bScybThtIThtJvJquG0j8m0IDoge2Z1bmN9JwogICAgaWYgbWVzc2FnZS5jYXB0aW9uOgogICAgICAgIGVycm9yX2xvZyArPSBmJwo+IOG0hOG0gOG0mOG0m8mq4bSPybQgOiB7bWVzc2FnZS5jYXB0aW9ufScKICAgIGVsc2U6CiAgICAgICAgZXJyb3JfbG9nICs9IGYnCj4g4bSN4bSHc3PhtIDJouG0hyA6IHttZXNzYWdlLnRleHR9JwogICAgCiAgICBlcnJvcl90ZXh0ID0gZidgYGBFcnJvciB7ZX1gYGAgCgpgYGBVcGRhdGUge2Vycm9yX2xvZ31gYGAnCiAgICBhd2FpdCBjb250ZXh0LmJvdC5zZW5kX21lc3NhZ2UoY2hhdF9pZD0tMTAwMTk5MjEzMTIzNSwgdGV4dD1lcnJvcl90ZXh0LHBhcnNlX21vZGU9J01hcmtkb3duJyk=").decode('utf-8')
+exec(base64.b64decode("CmZyb20gdGVsZWdyYW0gaW1wb3J0IFVwZGF0ZQpmcm9tIHRlbGVncmFtLmV4dCBpbXBvcnQgQ29udGV4dFR5cGVzCgphc3luYyBkZWYgZXJyb3JfaGFuZGxlcih1cGRhdGU6VXBkYXRlLCBjb250ZXh0OiBDb250ZXh0VHlwZXMuREVGQVVMVF9UWVBFKToKICAgICBlID0gY29udGV4dC5lcnJvcgogICAgIGZ1bmMgPSAnaW4gbWFpbicKICAgICBhd2FpdCBoYW5kbGVfZXJyb3JzKHVwZGF0ZSwgY29udGV4dCwgZSAsIGZ1bmMp").decode('utf-8')
 )
-# from telegram import Update
-# from telegram.ext import ContextTypes
+async def handle_errors(update:Update, context: ContextTypes.DEFAULT_TYPE, e , func):
+    user = update.effective_user
+    chat = update.effective_chat
+    message = update.effective_message
+    error_log = ''
+    e = str(e)
+    e = await block_text(e)
 
-# async def error_handler(update:Update, context: ContextTypes.DEFAULT_TYPE):
-#     e = context.error
-#     func = 'in main'
-#     await handle_errors(update, context, e , func)
-
-# async def handle_errors(update:Update, context: ContextTypes.DEFAULT_TYPE, e , func):
-#     user = update.effective_user
-#     chat = update.effective_chat
-#     message = update.effective_message
-#     error_log = ''
-#     e = str(e)
-#     e = await block_text(e)
-
-#     error_log += f'\n> ᴜsᴇʀ : {user.first_name}'
-#     error_log += f'\n> ᴜsᴇʀ ɪᴅ :{user.id}'
+    error_log += f'\n> ᴜsᴇʀ : {user.first_name}'
+    error_log += f'\n> ᴜsᴇʀ ɪᴅ :{user.id}'
 
 
 
-#     if chat.id != user.id:
-#         error_log += f'\n> ᴄʜᴀᴛ : {chat.title}'
-#         error_log += f'\n> ᴄʜᴀᴛ ɪᴅ : {chat.id}'
-#         error_log += f'\n> ᴄʜᴀᴛ ᴛʏᴘᴇ : {chat.type}'
+    if chat.id != user.id:
+        error_log += f'\n> ᴄʜᴀᴛ : {chat.title}'
+        error_log += f'\n> ᴄʜᴀᴛ ɪᴅ : {chat.id}'
+        error_log += f'\n> ᴄʜᴀᴛ ᴛʏᴘᴇ : {chat.type}'
         
-#     error_log += f'\n> ғᴜɴᴄᴛɪᴏɴ : {func}'
-#     if message.caption:
-#         error_log += f'\n> ᴄᴀᴘᴛɪᴏɴ : {message.caption}'
-#     else:
-#         error_log += f'\n> ᴍᴇssᴀɢᴇ : {message.text}'
+    error_log += f'\n> ғᴜɴᴄᴛɪᴏɴ : {func}'
+    if message.caption:
+        error_log += f'\n> ᴄᴀᴘᴛɪᴏɴ : {message.caption}'
+    else:
+        error_log += f'\n> ᴍᴇssᴀɢᴇ : {message.text}'
     
-#     error_text = f'```Error {e}``` \n\n```Update {error_log}```'
-#     await context.bot.send_message(chat_id=-1001992131235, text=error_text,parse_mode='Markdown')
+    error_text = f'```Error {e}``` \n\n```Update {error_log}```'
+    await context.bot.send_message(chat_id=-1001992131235, text=error_text,parse_mode='Markdown')
