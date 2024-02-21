1ccf56872bb89355b2cdb59cdc37b370851fe438
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-02-21 12:52:35 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -4,16 +4,17 @@ from Plugins import handle_song , start_command , help_command
 from config import TOKEN
 
 print('Starting up bot...')
-async def bad_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Raise an error to trigger the error handler."""
-    await context.bot.wrong_method_name()  # type: ignore[attr-defined]
+
+
+def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
 
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
-    # app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('song', handle_song))
-    app.add_handler(CommandHandler("error", bad_command))
-    app.add_error_handler(error_handler)
+    app.add_error_handler(error)
     print('Polling...')
-    app.run_polling(poll_interval=3)
+    app.run_polling(poll_interval=5)
+	
