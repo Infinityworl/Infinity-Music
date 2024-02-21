f6dfd5561208c90450908875b6d343c881499ed0
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-02-21 17:55:11 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -1,19 +1,19 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from Plugins import handle_song , start_command , help_command 
+from Plugins import start_command ,error_handler , song
 from config import TOKEN
 
 print('Starting up bot...')
-
-
-def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(f'Update {update} caused error {context.error}')
+async def bad_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Raise an error to trigger the error handler."""
+    await context.bot.wrong_method_name()  # type: ignore[attr-defined]
 
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
-    app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('song', handle_song))
-    app.add_error_handler(error)
+    # app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('song', song))
+    app.add_handler(CommandHandler("error", bad_command))
+    app.add_error_handler(error_handler)
     print('Polling...')
-    app.run_polling(poll_interval=5)
+    app.run_polling(poll_interval=3)
