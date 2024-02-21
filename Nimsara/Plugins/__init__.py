2e317617119a764f0346c8127551907f0278c78b
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-02-21 13:53:29 +0530

    Update __init__.py

--- Nimsara/Plugins/__init__.py
+++ Nimsara/Plugins/__init__.py
@@ -1,3 +1,3 @@
-from .start import start_command , mention
-from .error_handler import error_handler
-from .song import create_task_for_user as song
+from .song import *
+from .start import start_command
+from .help import help_command
