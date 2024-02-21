cadcb6f52d1d5acc2895458815715f70f79491d5
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-02-21 12:22:19 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
     # app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('song', song))
+    app.add_handler(CommandHandler('song', handle_song))
     app.add_handler(CommandHandler("error", bad_command))
     app.add_error_handler(error_handler)
     print('Polling...')
