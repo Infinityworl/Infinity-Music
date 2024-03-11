093557f426c5368de6807a510cb6004bdd38b90a
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-03-11 12:36:48 +0530

    Update __main__.py

--- Nimsara/__main__.py
+++ Nimsara/__main__.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-from Plugins import start_command ,error_handler, song
+from Plugins import start_command , error_handler, song, help_command
 from config import TOKEN
 print('Starting up bot...')
 async def bad_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -9,7 +9,7 @@ async def bad_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start_command))
-    # app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('song', song))
     app.add_handler(CommandHandler("error", bad_command))
     app.add_error_handler(error_handler)
